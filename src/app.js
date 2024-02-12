fe0e2984377e6614d198d7b31e79c05896a892c3
Brett <brett_neighbors@yahoo.com>
2024-02-12 13:48:22 -0700

    End of "Project Configuration" and "Application-level middleware" lessons. Comments explain each line of code.

---
+++ src/app.js
@@ -0,0 +1,44 @@
+
+// require express package and assign to variable. 
+const express = require("express");
+
+// require morgan package and assign to variable
+const morgan = require("morgan");
+
+// Express package exports a function
+// when function is invoked, you get a new Express application. 
+// Assing that express app to a variable. 
+const app = express();
+
+
+// middleware function
+// takes 3 arguments
+// req = request. Information and methods related to the incoming request will be stored in this object.
+// res = response. This object has information and methods related to sending back a response from the server.
+// next = next. tells Express that this middleware function is complete. It will then go on to the next piece of middleware.
+const sayHello = (req, res, next) => {
+    res.send("Hello!")
+}
+
+// Replace logging function below
+// morgan will log incoming requests like: "GET / 304 3.881 ms - - GET /favicon.ico 304 0.519 ms - -"
+app.use(morgan("dev"));
+
+/*
+// nonresponsive middleware function
+// next() function called to go to the next middleware function
+const logging = (req, res, next) => {
+    console.log("A request is being made!");
+    next();
+  };
+*/
+
+
+// logging needs to be put first due to the fact that sayHello does not have a next() fx inside it. 
+// with no next() inside, the middleware will not go to the next middleware fx
+// app.use(logging);
+app.use(sayHello);
+
+
+// export Express app to be used in other files
+module.exports = app; 
---
+++ src/server.js
@@ -0,0 +1,22 @@
+// create port and set it = whatever is in process.env (set default to 5000)
+const {PORT = 8000} = process.env;
+
+// Require Express application (app) that was created in app.js
+const app = require("./app");
+
+// This function will run when the server successfully starts.
+const listener = () => {
+    console.log(`The server is live on Port ${PORT}`);
+};
+
+/* 
+The listen() method on your Express application is what runs the server. It takes two arguments: 
+a port number and a function. The PORT variable defines where your server is running, and the 
+listener() function will get called as soon as the server has successfully started.
+*/
+
+// listen() method (app.listen()) is what runs the server
+// It takes 2 arguments: a PORT number, and a function
+// PORT defines where your server is running
+// listener() function will get called as soon as server has successfully started
+app.listen(PORT, listener);
